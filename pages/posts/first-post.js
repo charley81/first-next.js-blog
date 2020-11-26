881d712129627df3c5f443bf8d1f2d704a73d38e
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-26 17:41:55 -0500

    added head to first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const firstPost = () => {
   return (
     <div>
+    <Head>
+        <title>First blog post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h3>First Post</h3>
       <p>
         <Link href="/"><a>Back Home</a></Link>
